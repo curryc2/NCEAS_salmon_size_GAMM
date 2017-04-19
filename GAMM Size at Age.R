dca839c163f126fb79b408fd7adbcca9ba28c335
Curry Cunningham <cunninghamcurry@gmail.com>
2017-04-19 15:42:20 -0700

    Created R project

---
+++ GAMM Size at Age.R
@@ -0,0 +1,20 @@
+#==================================================================================================
+#Project Name: NCEAS SIZE AT AGE - Generalized Additive Models
+#Creator: Curry James Cunningham, NOAA/NMFS, ABL
+#Date: 4.19.17
+#
+#Purpose: Evaluate Changes in Size at Age Using Generalized Additive Mixed Models
+#
+# 1) Read in data using FishData from throson
+#
+#
+#==================================================================================================
+#NOTES:
+#  a) 
+#
+#==================================================================================================
+
+
+
+
+
