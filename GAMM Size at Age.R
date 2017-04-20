3e91086ffc364baed6b5c810e9554374c28347c7
Curry Cunningham <cunninghamcurry@gmail.com>
2017-04-20 13:40:00 -0700

    Update

--- GAMM Size at Age.R
+++ GAMM Size at Age.R
@@ -14,8 +14,99 @@
 #
 #==================================================================================================
 require(mgcv)
+require(ggplot2)
+require(fda)
+require(dplyr)
+require(gamm4)
 
 
+#Read in data
+asl <- read.csv('Data/asl_size_at_age.csv', header=TRUE, stringsAsFactors=FALSE)
+head(asl)
+
+
+#Metadata
+regions <- sort(unique(asl$SASAP.Region))
+n.regions <- length(regions)
+
+unique(asl$ASLProjectType)
+
+
+#Aggreagate up to Region level (just for fun)
+
+asl.agg <- data.frame(asl %>% group_by(SASAP.Region, Year, SWage, Species) %>% summarize(mean=mean(Length_mean, na.rm=TRUE)) %>% filter(mean!='NaN'))
+
+asl.agg <- na.omit(asl.agg) #Remove NA's
+#Example GAMM
+
+g <- ggplot(asl.agg, aes(x=Year, y=mean, color=SASAP.Region)) +
+       theme_gray() +
+       geom_point() +
+       facet_grid(Species~SWage)
+g
+
+
+
+#Basic GAM
+gam.1 <- gam(mean~s(Year, bs='cr') + SWage + Species, data=asl.agg)
+summary(gam.1)
+
+acf(residuals(gam.1,type="response"),main="standardized residual ACF")
+
+
+
+#GAMM with Region as RE'
+
+
+
+gamm.1 <- gamm4(mean~s(Year, bs='cr') + SWage + Species, random=~(1|SASAP.Region), data=asl.agg)
+
+
+gamm.2 <- gamm4(mean~s(Year, bs='cr') + SWage + Species, random=~(Year|SASAP.Region), data=asl.agg)
+
+
+
+
+# 
+# gam.1 <- gam(mean~s(Year, bs='cr'), correlation=corAR1(), data=asl.agg)
+# 
+# 
+# trial.1 <- gamm(mean ~ s(Year, bs='cr'), random=list(data=asl.agg))
+#                 
+#                 
+#               
+
+
+#Creation of fourier baiss
+years <- c(min(asl.agg$Year, na.rm=TRUE):max(asl.agg$Year, na.rm=TRUE))
+n.years <- length(years)
+fbs <- create.fourier.basis(rangeval=c(min(years),max(years)), n.basis=)
+plot(fbs)  
+         
+
+
+
+
+ys <- smooth.basis(argvals=as.numeric(asl.agg$Year), y=asl.agg$mean, fdParobj=fbs)
+
+plot(ys)
+
+
+
+tempfd3 <- with(CanadianWeather, smooth.basis(
+  day.5, dailyAv[,,"Temperature.C"],
+  daybasis3, fdnames=list("Day", "Station", "Deg C"))$fd )
+
+
+
+
+precfd5 <- with(CanadianWeather, smooth.basis(
+  day.5, dailyAv[,,"log10precip"],
+  daybasis5, fdnames=list("Day", "Station", "Deg C"))$fd )
+# Correlation matrix for a single functional data object
+(tempCor3 <- cor.fd(seq(0, 356, length=4), tempfd3))
+# Cross correlation matrix between two functional data objects
+# Compare with structure described above under 'value':
 
 
 
---
+++ Simulate Bias.R
@@ -0,0 +1,86 @@
+#==================================================================================================
+#Project Name: NCEAS SIZE AT AGE - Generalized Additive Models
+#Creator: Curry James Cunningham, NOAA/NMFS, ABL
+#Date: 4.19.17
+#
+#Purpose: Evaluate Changes in Size at Age Using Generalized Additive Mixed Models
+#
+# 1) Read in data using FishData from throson
+#
+#
+#==================================================================================================
+#NOTES:
+#  a) 
+#
+#==================================================================================================
+
+
+days <- 150:240
+n.days <- length(days)
+
+
+
+#Distribution parameters
+mu <- 200
+sd <- 25
+
+proc.sigma <- 0.2
+
+
+#Parameters for trend across season
+int <- 550
+slp <- -2
+
+#Generate size trend
+size.trend <- int + slp*(days-mean(days))
+
+#Define sampling periods
+
+
+dates.pds <- matrix(c(160,170,
+                      175,180,
+                      185,190,
+                      200,220), nrow=4, ncol=2, byrow=TRUE)
+
+n.pds <- nrow(dates.pds)
+
+#Samples per period
+n.samps <- c(100,100,100,100)
+
+if(length(n.samps)!=n.pds) { stop('#Number of Sampling periods does not equal length of sample sizes') }
+
+
+#Arrival trend
+N.dist <- dnorm(days, mu, sd)
+N <- N.dist * rlnorm(n.days, 0-((proc.sigma^2)/2), proc.sigma)
+
+
+#Plot Arrival and sampling Process
+y.lim <- c(min(N.dist,N),max(N.dist,N))
+plot(x=days, y=N.dist, type='l', col='red', ylim=y.lim)
+points(x=days, y=N, pch=21, bg=rgb(1,0,0,alpha=0.2))
+
+#Plot the blocks
+i <- 1
+for(i in 1:n.pds) {
+  polygon(x=c() , y= )
+}
+
+errbar ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
